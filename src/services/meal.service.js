181d362b3c4aad42e47cba0d644db56a52e78e1c
lauraamelina <lauraamelina@gmail.com>
2024-05-30 15:50:16 -0300

    feat: add service meal

---
+++ src/services/meal.service.js
@@ -0,0 +1,16 @@
+const URL = 'https://www.themealdb.com/api/json/v1/1/'
+
+async function getMealsByName(name) {
+    return fetch(URL + 'search.php?s=' + name)
+        .then(response => response.json())
+}
+
+async function getMealsByFirstLetter(letter) {
+    return fetch(URL + 'search.php?f=' + letter)
+        .then(response => response.json())
+}
+
+export {
+    getMealsByName,
+    getMealsByFirstLetter
+}
