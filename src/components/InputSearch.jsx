a63eb056873d309ab572d5a4f88bdeed3d4c8d99
lauraamelina <lauraamelina@gmail.com>
2024-05-31 16:22:05 -0300

    styles: add styles InputSearch Component

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function InputSearch({ search, setSearch }) {
     return (
         <form>
-            <input className="form-control" type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+            <input className="form-control search-input" type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
             <p>{search}</p>
         </form>
     )
