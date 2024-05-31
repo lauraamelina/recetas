1b2f0a04147f3dcd0983769e64f0470cfd7d9c19
lauraamelina <lauraamelina@gmail.com>
2024-05-31 18:27:11 -0300

    styles: modified color Pagination

--- src/components/inputs/InputSearch.jsx
+++ src/components/inputs/InputSearch.jsx
@@ -4,7 +4,6 @@ function InputSearch({ search, setSearch }) {
     return (
         <form>
             <input className="form-control search-input" type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Buscá tu receta favorita" />
-            <p>{search}</p>
         </form>
     )
 }
