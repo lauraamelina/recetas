1857f91b10eda9ccefca51fa6baaf64bac630ab2
lauraamelina <lauraamelina@gmail.com>
2024-06-01 16:54:52 -0300

    feat: configure React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.css';
-import './assets/css/styles.css';
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/css/styles.css";
+import { Routes, Route } from "react-router-dom";
+import PageMealById from "./pages/PageMealById";
 
 function App() {
-    return ( <
-        Home / >
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/receta/:id" element={<PageMealById />} />
+        </Routes>
     );
 }
 
