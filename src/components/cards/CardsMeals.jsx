b05aafb87733ff82032520cdb25bbb5769407ce8
lauraamelina <lauraamelina@gmail.com>
2024-06-03 14:09:10 -0300

    feat: add service SavedMeals to CardsMeals

--- src/components/cards/CardsMeals.jsx
+++ src/components/cards/CardsMeals.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CardMeal from './CardMeal'
 
-function CardsMeals({ meals }) {
+function CardsMeals({ meals, updateSavedMeals }) {
   return (
     <div className="meals">
       <div className="bg">
         <p>EpicEats</p>
       </div>
       {meals && meals?.map((meal) =>
-        <CardMeal meal={meal} key={meal.idMeal} />
+        <CardMeal meal={meal} key={meal.idMeal} updateSavedMeals={updateSavedMeals} />
       )}
     </div>
   );
