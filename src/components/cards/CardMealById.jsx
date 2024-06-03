6f8a0fb1b340675bafc29bf2fbd956a4094f218a
lauraamelina <lauraamelina@gmail.com>
2024-06-03 10:20:57 -0300

    add recipe information to CardMealById

--- src/components/cards/CardMealById.jsx
+++ src/components/cards/CardMealById.jsx
@@ -23,29 +23,37 @@ export default function CardMealById({ meal }) {
         return ingredientsWithMeasures;
     }
     return (
-        <div className="mealById">
-            <h2>{meal.strMeal}</h2>
-            <p>Categoría: {meal.strCategory}</p>
-            <p>Área: {meal.strArea}</p>
-            <div className="ingredientes">
-                <h3>Ingredients</h3>
-                <ul>
-                    {getIngredientsWithMeasures(meal).map((item, index) => (
-                        <li key={index}>
-                            {item.ingredient} {item.measure}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="instrucciones">
-                <h3>Instrucciones</h3>
-                <p>{meal.strInstructions}</p>
-            </div>
-            <div className="imagen">
-                <img
-                    src={meal.strMealThumb}
-                    alt={meal.strMeal}
-                />
+        <div className="mealById conteiner">
+            <h2 className="visually-hidden">{meal.strMeal}</h2>
+            <div className="mealContent row">
+                <div className="info col-md-8">
+                    <p className="title">{meal.strMeal}</p>
+                    <p className="badge bg-secondary">{meal.strCategory}</p>
+                    <p className="badge bg-secondary">{meal.strArea}</p>
+                    <div className="row">
+                        <div className="ingredientes col-md-3">
+                            <h3>Ingredientes</h3>
+                            <ul>
+                                {getIngredientsWithMeasures(meal).map((item, index) => (
+                                    <li key={index}>
+                                        <span className="fw-bold">{item.ingredient} </span>
+                                        {item.measure}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="instrucciones col-md-8">
+                            <h3>Instrucciones</h3>
+                            <p>{meal.strInstructions}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="imagen col-md-4">
+                    <img
+                        src={meal.strMealThumb}
+                        alt={meal.strMeal}
+                    />
+                </div>
             </div>
         </div>
     )
