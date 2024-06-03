8a56e363de56950737fdfe8477421f0386457199
lauraamelina <lauraamelina@gmail.com>
2024-06-03 14:03:45 -0300

    feat: add route SavedMeals

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/styles.css";
 import { Routes, Route } from "react-router-dom";
 import PageMealById from "./pages/PageMealById";
+import PageMealSaved from "./pages/PageMealSaved";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/receta/:id" element={<PageMealById />} />
+            <Route path="/recetas/guardados" element={<PageMealSaved />} />
         </Routes>
     );
 }
